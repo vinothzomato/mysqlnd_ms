b812daf358d8d552c6b773592ca5f1d8ea7b4d88
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-06-03 15:21:11 +0000

    move random filter to a separate file
    git-svn-id: https://svn.php.net/repository/pecl/mysqlnd_ms/trunk@311774 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ mysqlnd_ms_filter_random.h
@@ -0,0 +1,36 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2008 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Andrey Hristov <andrey@php.net>                              |
+  |         Ulf Wendel <uw@php.net>                                      |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id: mysqlnd_ms.h 311510 2011-05-27 19:36:39Z andrey $ */
+#ifndef MYSQLND_MS_FILTER_RANDOM_H
+#define MYSQLND_MS_FILTER_RANDOM_H
+
+MYSQLND * mysqlnd_ms_choose_connection_random(const char * const query, const size_t query_len, struct mysqlnd_ms_lb_strategies * stgy, zend_llist * master_connections, zend_llist * slave_connections TSRMLS_DC);
+
+#endif	/* MYSQLND_MS_FILTER_RANDOM_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
